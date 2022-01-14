4575964f93dbc80f1fdce98ae8c41e7501911173
Kasukana Sora <waggonernick02@gmail.com>
2022-01-14 00:40:43 -0500

    Created Command File For User
    Command File For Specific User to Interact With Their Members

---
+++ Commands/jackie.js
@@ -0,0 +1,5 @@
+module.exports = {
+case 'jackie' :
+        message.author.send("Join the Realm Bros. Minecraft Realm today! https://realms.gg/-exmBdxOAtc");
+        message.channel.send("Check your DMs!");
+};
